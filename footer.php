ba71d591089588abfa331b00abaf0a42abcf8be0
Alex <alcouch65@gmail.com>
2016-02-16 09:46:12 -0800

    Created Registry

---
+++ footer.php
@@ -0,0 +1,13 @@
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container">
+            <div class="btn-toolbar">
+                <div class="btn-group" id="buttons">
+                    <button class="btn btn-large btn-primary" href="#" type="button" >Cook</button>
+                    <button class="btn btn-large" href="#">Buy</button>
+                    <button class="btn btn-large" href="#">Recipe</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ shoplist.js
@@ -0,0 +1,3 @@
+$(".list-body").onmouseover(function(){
+   $(this).css("box-shadow", "5px 5px 5px #000");
+});
