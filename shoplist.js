23c567b1a1c3fca80aca5f44d434e1d638dddc21
Alex <alcouch65@gmail.com>
2016-02-17 09:42:11 -0800

    Javascript added

--- shoplist.js
+++ shoplist.js
@@ -1,3 +1,7 @@
+function addItems(val){
+   document.html("<li class='list-body'>"+val+"</li>");
+}
+
 $(document).ready(function(){
    $(".list-body").mouseenter(function(){
       $(this).css("box-shadow", "5px 5px 5px #000");
