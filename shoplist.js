c25874854a0c91a142ee582a5e30c843952f5a1d
alex-couch <tempus.studios.contact@gmail.com>
2016-02-17 18:43:17 -0800

    Created the main html file for testing the domain url

--- shoplist.js
+++ shoplist.js
@@ -1,7 +1,3 @@
-function addItems(val){
-   document.html("<li class='list-body'>"+val+"</li>");
-}
-
 $(document).ready(function(){
    $(".list-body").mouseenter(function(){
       $(this).css("box-shadow", "5px 5px 5px #000");
