0ab931fe39195ce224b4a00e85df199f10be685b
alex-couch <tempus.studios.contact@gmail.com>
2016-02-18 22:04:29 -0800

    Created a local storage, fixed some javascript, and added some graphical features

--- shoplist.js
+++ shoplist.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-    $("#item").onkeydown = function(e){
-        if(e.keyCode == 13){
+    $("input").keypressed = function(e){
+        if(e.which == 13){
+            e.preventDefault();
             submitItem();
         }
     };
@@ -14,13 +15,28 @@ $(document).ready(function(){
         $(".list-body").css("box-shadow", "10ox 10px 5px #000");
     });
 
-    $(".list-body").on('click', '#remove', function(){
-        $(this).remove();
+    $(document).on("click", ".close", function(){
+        $(this).parent().fadeOut();
     });
+
+    $(document).on("click", ".clear", function(){
+        deleteAllItems();
+    });
+
+    if(localStorage.getItem('item')){
+        $("#list").html(localStorage.getItem('item'));
+    }
 });
 
 function submitItem(){
     var itemName = $("#item").val();
-    $("#list").append("<li class='list-body'>"+itemName+"<button type='button' id='remove' class='close'>&times;</button></li>");
+    var items = $("#list").html();
+    $("#list").append("<li class='list-body'>"+itemName+"<button type='button' class='close'>X</li>");
     $("#item").val('');
+    localStorage.setItem("item", items);
+}
+
+function deleteAllItems(){
+    localStorage.clear();
+    location.reload();
 }
